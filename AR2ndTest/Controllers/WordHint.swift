220b61e6f0512db83a44b92852f4fa10c55760e3
khairunnishaafifa <khairunnishaliyyafifa@gmail.com>
2021-08-08 14:59:01 +0700

    refactor word hint

--- AR2ndTest/Controllers/WordHint.swift
+++ AR2ndTest/Controllers/WordHint.swift
@@ -14,12 +14,14 @@ class WordHint: UIViewController {
     @IBOutlet weak var prevButton: UIButton!
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var imageHint: UIImageView!
+    
     var wordSupposedToBe:String = ""
     var dash:String = ""
     var word:String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         AudioBGM.shared.playSound()
 
         let currentAlphabet = UserDefaults.standard.string(forKey: "currentAlphabet")!
@@ -34,6 +36,7 @@ class WordHint: UIViewController {
     
     func getValueForRecognition(alphabetName:String) -> String{
         var alphabetToRecognize:[AlphabetTable]?
+        
         do {
             let request = AlphabetTable.fetchRequest() as NSFetchRequest<AlphabetTable>
             let pred = NSPredicate(format: "alphabet == %@", alphabetName)
@@ -42,20 +45,22 @@ class WordHint: UIViewController {
         } catch {
             print(error.localizedDescription)
         }
+        
         return alphabetToRecognize![0].wordRec!
     }
     
     @IBAction func pauseButtTapped(_ sender: Any) {
         AudioPausedTheme.shared.playSound()
     }
+    
     @IBAction func prevScreen(_ sender: Any) {
         AudioPrevTapped.shared.playSound()
+        
         dismiss(animated: true, completion: nil)
     }
+    
     @IBAction func nextButtTapped(_ sender: Any) {
         AudioBGM.shared.stopSound()
         AudioNextTapped.shared.playSound()
     }
-    
-
 }
