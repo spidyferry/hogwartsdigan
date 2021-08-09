e5653936cdc20247056e0508584262f1f8973b43
spidyferry <1988ferry@gmail.com>
2021-08-09 13:33:58 +0700

    Play pause audio dah OK

--- AR2ndTest/Controllers/AlphabetSuccess.swift
+++ AR2ndTest/Controllers/AlphabetSuccess.swift
@@ -9,13 +9,13 @@ import UIKit
 
 class AlphabetSuccess: UIViewController {
 
+    @IBOutlet weak var headerTitle: UILabel!
     @IBOutlet weak var bodyText: UITextView!
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var whiteTransBg: UIView!
     @IBOutlet weak var replayButton: UIButton!
     
-    var countTimer = 0
-    var timer = Timer()
+    var alphabetSuccess: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,42 +23,30 @@ class AlphabetSuccess: UIViewController {
         AudioCongrat.shared.playSound()
         AudioBGM.shared.playSound()
         
-//        let alphabetSuccess:String = UserDefaults.standard.string(forKey: "alphabetSuccess")!
-//        let line = self.loadNarrationText(file: alphabetSuccess)
-//        let sentence = line.split(separator: ";").map {String($0)}
-//        startNaration(script: sentence)
+        self.alphabetSuccess = UserDefaults.standard.string(forKey: "alphabetSuccess")!
+        self.startNaration()
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(getSentece),userInfo: nil,repeats: true)
-        timer.fire()
-//        startNaration(script: sentence)
+        AudioBGM.shared.startSound()
     }
     
-    @objc
-    func getSentece(){
-        countTimer+=1
+    func startNaration() {
+        let line = self.loadNarrationText(file: self.alphabetSuccess)
+        var sentence = line.split(separator: ";").map {String($0)}
         
-        let alphabetIntro:String = UserDefaults.standard.string(forKey: "alphabetIntro")!
-        let line = self.loadNarrationText(file: alphabetIntro)
-        let sentence = line.split(separator: ";").map {String($0)}
-        if countTimer/3 < sentence.count{
-            self.bodyText.text = sentence[countTimer/3]
+        sentence.removeLast()
+        
+        for (index, element) in sentence.enumerated() {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0*Double(index)) {
+                self.bodyText.text = element
+                
+                if index == (sentence.count - 1) {
+                    self.replayButton.isHidden = false
+                }
+            }
         }
-//        if countTimer/3 > sentence.count{
-//            repeatButton.isHidden = false
-//        }
     }
-    
-//    func startNaration(script:Array<String>){
-//        for (index, element) in script.enumerated() {
-//          print("Item \(index): \(element)")
-//
-//            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0*Double(index)) {
-//                self.bodyText.text = script[index]
-//            }
-//        }
-//    }
     
     func loadNarrationText(file:String) -> String{
         guard let path = Bundle.main.path(forResource: file, ofType: "txt"),
@@ -70,6 +58,7 @@ class AlphabetSuccess: UIViewController {
     @IBAction func pauseMenuTapped(_ sender: Any) {
         AudioNextTapped.shared.playSound()
         AudioPausedTheme.shared.playSound()
+        AudioBGM.shared.pauseSound()
     }
     
     @IBAction func nextButtonTapped(_ sender: Any) {
@@ -78,6 +67,6 @@ class AlphabetSuccess: UIViewController {
     
     @IBAction func replayButtonTapped(_ sender: Any) {
         self.replayButton.isHidden = true
-//        self.startNaration()
+        self.startNaration()
     }
 }
